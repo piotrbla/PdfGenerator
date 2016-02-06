644607490e509e43f88db7bb5c7ffb008eeaaa00
PiotrB <piotrb@gmail.com>
2016-02-06 18:21:13 +0100

    Working code

---
+++ PdfGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using ColorCode;
+
+namespace PdfGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceCode = File.ReadAllText(@"../../Program.cs");
+            var colorizedSourceCode = new CodeColorizer().Colorize(sourceCode, Languages.CSharp);
+            
+            Console.WriteLine(colorizedSourceCode);
+        }
+    }
+}
